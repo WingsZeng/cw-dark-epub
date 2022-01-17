48ac67cdf0b8c43f5d08bca27233564735a568eb
WingsZeng <wings.xiangyi.zeng@gmail.com>
2022-01-17 11:49:45 +0800

    add simple fontsize adjustment

--- js/reading/epub_theme.js
+++ js/reading/epub_theme.js
@@ -5,16 +5,24 @@ var themes = {
 
 var dark_mode = window.localStorage.getItem("dark_mode") === "y";
 var theme = dark_mode ? "dark" : "light";
+var fontsz = window.localStorage.getItem("fontsize");
+fontsz = fontsz ? Number(fontsz) : 16;
 
 var e_css = "<link rel=\"stylesheet\" href=\"/static/css/epub_themes/base_page.css\">";
+/* TODO: use svg icon*/
 var e_switch = "<a id=\"theme\" class=\"icon-theme\">✾</a>";
+var e_fontsz_inc = "<a id=\"fontsz_inc\" class=\"icon-fontsz\">+</a>";
+var e_fontsz_dec = "<a id=\"fontsz_dec\" class=\"icon-fontsz\">-</a>";
 
 $("head").append(e_css);
 $("#title-controls").prepend(e_switch);
+$("#title-controls").prepend(e_fontsz_dec);
+$("#title-controls").prepend(e_fontsz_inc);
 
 reader.rendition.themes.register("light", themes["light"]);
 reader.rendition.themes.register("dark", themes["dark"]);
 reader.rendition.themes.default(themes[theme]);
+reader.rendition.themes.fontSize(fontsz);
 switchBasePageTheme(theme);
 
 function switchEpubjsTheme(theme) {
@@ -40,8 +48,23 @@ function switchTheme(theme) {
     switchBasePageTheme(theme);
 }
 
+function changeFontSize(size) {
+    reader.rendition.themes.fontSize(size + 'px');
+    window.localStorage.setItem("fontsize", size.toString());
+}
+
 $("#theme").click(function() {
     dark_mode = !dark_mode;
     switchTheme(dark_mode ? "dark" : "light");
     window.localStorage.setItem("dark_mode", dark_mode ? "y" : "n");
+});
+
+$("#fontsz_inc").click(function () {
+    fontsz += fontsz < 72 ? 2 : 0;
+    changeFontSize(fontsz);
+});
+
+$("#fontsz_dec").click(function () {
+    fontsz -= fontsz > 4 ? 2 : 0;
+    changeFontSize(fontsz);
 });
